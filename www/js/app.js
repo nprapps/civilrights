d7d6959b3e59b8233d995ec3098630f18fdaa76b
Danny DeBelius <danny.debelius@gmail.com>
2014-06-18 15:49:39 -0400

    Bill styling, add scrollTo

--- www/js/app.js
+++ www/js/app.js
@@ -1,2 +1,12 @@
+var $chapterLinks = null;
+
+var onChapterClick = function(){
+	var target = $(this).attr('href');
+	$.scrollTo($(target), { duration: 350 });
+}
+
 $(function() {
+	$chapterLinks = $('.chapter-nav a');
+
+	$chapterLinks.on('click', onChapterClick);
 });
