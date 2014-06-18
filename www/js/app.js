45d0a15ac138815f7ed0a2e191b60d637184430d
Danny DeBelius <danny.debelius@gmail.com>
2014-06-18 17:21:44 -0400

    Apply affix to bill chapter nav

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,26 @@
 var $chapterLinks = null;
 
-var onChapterClick = function(){
+var onChapterClick = function() {
 	var target = $(this).attr('href');
 	$.scrollTo($(target), { duration: 350 });
+}
+
+var setupChapterAffix = function() {
+	if (Modernizr.mq('only screen and (min-width: 992px)')){
+		$('.chapter-nav ul').affix({
+			offset: {
+				top: function () {
+					return (this.top = $('.chapter-nav').offset().top + 11)
+				}
+			}
+		})
+	}
 }
 
 $(function() {
 	$chapterLinks = $('.chapter-nav a');
 
 	$chapterLinks.on('click', onChapterClick);
+
+	setupChapterAffix();
 });
