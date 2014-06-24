d53c7d8e6da6a018fa0689b03d43c88391de5727
Danny DeBelius <danny.debelius@gmail.com>
2014-06-24 15:06:58 -0400

    New mode toggle layout

--- www/js/app.js
+++ www/js/app.js
@@ -1,28 +1,63 @@
+var $body = null;
 var $chapterLinks = null;
 var $billLinks = null;
 var $annotationLinks = null;
+var $bill = null;
+var $annotations = null;
+var $fullTextButton = null;
+var mode = 'annotations';
 
 var onChapterClick = function(e) {
 	e.preventDefault();
 
 	var target = $(this).attr('href');
-	$.scrollTo($(target), { duration: 350 });
+	$(target).velocity("scroll", { duration: 500, offset: -60});
 }
 
 var onBillLinkClick = function(e) {
 	e.preventDefault();
+	mode = 'fullText';
+	$body.removeClass().addClass('fulltext-active');
+
+	if ($(this).hasClass('active')){
+		return;
+	}
 
 	var target = $(this).attr('href');
-	$('#tab-nav a[href="#bill"]').tab('show');
-	$.scrollTo($(target), { duration: 350 });
+	$bill.velocity("fadeIn", { duration: 300 });
+	$annotations.velocity("fadeOut", {
+		duration: 300,
+		complete: function(){
+			setupChapterAffix();
+			$(target).velocity("scroll", { duration: 500, offset: -60, easing: "ease-in-out" }) ;
+		}
+	});
+
+	$('.mode .toggle').removeClass('active');
+	$('.toggle.bill-link').addClass('active');
 }
 
 var onAnnotationLinkClick = function(e) {
 	e.preventDefault();
+	mode = 'annotations';
+	$body.removeClass().addClass('annotations-active');
+
+	if ($(this).hasClass('active')){
+		return;
+	}
 
 	var target = $(this).attr('href');
-	$('#tab-nav a[href="#annotations"]').tab('show');
-	$.scrollTo($(target), { duration: 350 });
+
+	$('.mode .toggle').removeClass('active');
+	$('.toggle.annotation-link').addClass('active');
+
+	$annotations.velocity("fadeIn", { duration: 300 });
+	$bill.velocity("fadeOut", {
+		duration: 300,
+		complete: function(){
+			$(target).velocity("scroll", { duration: 500, offset: -60, easing: "ease-in-out" }) ;
+		}
+	});
 }
 
 var setupChapterAffix = function() {
@@ -30,7 +65,7 @@ var setupChapterAffix = function() {
 		$('.chapter-nav ul').affix({
 			offset: {
 				top: function () {
-					return (this.top = $('.chapter-nav').offset().top - 11)
+					return (this.top = $('.chapter-nav').offset().top - 71)
 				},
  				bottom: function () {
  					return (this.bottom = $(document).height() - $('footer').offset().top + 11)
@@ -40,15 +75,40 @@ var setupChapterAffix = function() {
 	}
 }
 
+var onWindowResize = function(){
+	$('header').css('height', $(window).height());
+	$('body').scrollspy({ target: '.chapter-nav', offset: 60 });
+
+	if ( $fullTextButton.hasClass('active') ) {
+		setupChapterAffix();
+	}
+}
+
+var onDocumentScroll = function() {
+	var scrollPercentage = $(window).scrollTop() / $(window).height()
+
+	if (mode === 'annotations' && scrollPercentage > 1){
+		$body.addClass('show-title');
+	} else {
+		$body.removeClass('show-title');
+	}
+}
+
 $(function() {
+	$body = $('body');
 	$chapterLinks = $('.chapter-nav a');
 	$billLinks = $('.bill-link');
 	$annotationLinks = $('.annotation-link');
+	$bill = $('#bill');
+	$annotations = $('#annotations');
+	$fullTextButton = $('.bill-link.toggle');
 
 	$chapterLinks.on('click', onChapterClick);
 	$billLinks.on('click', onBillLinkClick);
 	$annotationLinks.on('click', onAnnotationLinkClick);
 
-	setupChapterAffix();
-	$('body').scrollspy({ target: '.chapter-nav' })
+	$(window).on('resize', _.throttle(onWindowResize, 300));
+	$(document).on('scroll', _.throttle(onDocumentScroll, 300));
+
+	onWindowResize();
 });
